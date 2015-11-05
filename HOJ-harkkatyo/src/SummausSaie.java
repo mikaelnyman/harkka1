fad7c9645edca1bd2d398e5e1ca700d82db2b8be
Josia Nyman <josia.nyman@gmail.com>
2015-11-05 14:23:58 +0200

    Turhat metodit pois

--- HOJ-harkkatyo/src/SummausSaie.java
+++ HOJ-harkkatyo/src/SummausSaie.java
@@ -5,39 +5,6 @@ public class SummausSaie extends Thread {
 		// TODO Auto-generated constructor stub
 	}
 
-	public SummausSaie(Runnable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(ThreadGroup arg0, Runnable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(ThreadGroup arg0, String arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(Runnable arg0, String arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(ThreadGroup arg0, Runnable arg1, String arg2) {
-		super(arg0, arg1, arg2);
-		// TODO Auto-generated constructor stub
-	}
-
-	public SummausSaie(ThreadGroup arg0, Runnable arg1, String arg2, long arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
