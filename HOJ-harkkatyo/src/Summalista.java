24bcc44630254684652cecb0c2d6a4548b67bc53
Josia Nyman <josia.nyman@gmail.com>
2015-11-09 19:25:38 +0200

    Kommentit javadoc-muotoon

--- HOJ-harkkatyo/src/Summalista.java
+++ HOJ-harkkatyo/src/Summalista.java
@@ -5,26 +5,31 @@ public class Summalista
 	private int summa = 0;
 	private int alkiot = 0;
 	
-	//Konstruktori, joka luo uuden Summalista-olion.
+	/*
+	 * Konstruktori, joka luo uuden Summalista-olion.
+	 */
 	public Summalista()
 	{
 		this.summa = 0;
 		this.alkiot = 0;
 	}
 	
-	//Metodi lis‰‰ parametrina annetun int tyypin luvun
-	// listassa olevaan summaan ja kasvattaa lis‰ttyjen
-	// lukujen lukum‰‰r‰‰ yhdell‰.
+	/*
+	 * Metodi lis‰‰ parametrina annetun int tyypin luvun
+	 * listassa olevaan summaan ja kasvattaa lis‰ttyjen
+	 * lukujen lukum‰‰r‰‰ yhdell‰.
+	 */
 	public void setUusiluku(int x)
 	{
 		this.summa = summa+x;
 		this.alkiot = alkiot+1;
 	}
 	
-	//Metodi palauttaa listassa olevan summan ja sinne
-	// lis‰ttyjen lukujen lukum‰‰r‰n ArrayList-oliossa.
-	//Palautettavan listan ensimm‰inen alkio on alkioiden
-	//summa ja toinen alkio on alkioiden lukum‰‰r‰.
+	/*
+	 * Metodi palauttaa listassa olevan summan ja sinne lis‰ttyjen lukujen
+	 * lukum‰‰r‰n int[]-taulukossa. Palautettavan listan ensimm‰inen alkio on
+	 * alkioiden summa ja toinen alkio on alkioiden lukum‰‰r‰.
+	 */
 	public int[] getSisalto()
 	{
 		int[] Arvot = {this.summa,this.alkiot};
