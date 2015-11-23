294515f7df03524f2744f1fae14826700d804c42
Mikael <mikaelnyman92@gmail.com>
2015-11-23 19:54:06 +0200

    Pitäis olla aika valmis

--- HOJ-harkkatyo/src/SummausPalvelin.java
+++ HOJ-harkkatyo/src/SummausPalvelin.java
@@ -14,25 +14,29 @@ public class SummausPalvelin extends Thread {
 	/*
 	 * Tietokent‰t
 	 */
-	//Portti, johon muodostetaan UDP. Annettu teht‰v‰nannossa.
-	private final int yPortti=3126;
-	//Keksitty porttinumero, johon palvelin Y muodostaa TCP-yhteyden
-	private final int zPortti=2000;
-	//Y:lt‰ saatava Summaus‰ikeiden lukum‰‰r‰
-	private Summalista[] summaTaulukko=null;
+	// Portti, johon muodostetaan UDP. Annettu teht‰v‰nannossa.
+	private final int YPORTTI;
+	// Keksitty porttinumero, johon palvelin Y muodostaa TCP-yhteyden
+	private final int ZPORTTI;
+	// Y:lt‰ saatava Summaus‰ikeiden lukum‰‰r‰
+	private Summalista[] summaTaulukko;
 	// Lista, jossa summauspalvelijat ovat
-	private SummausSaie[] ss=null;
-	private Socket asiakasSoketti=null;
-	private ObjectOutputStream oVirta=null;
-	private ObjectInputStream iVirta=null;
+	private SummausSaie[] ss;
+	// Yhteys Y:hyn TCP:t‰ muodostettaessa
+	private Socket asiakasSoketti;
+	// X:lt‰ Y:lle
+	private ObjectOutputStream oVirta;
+	// Y:lt‰ X:lle
+	private ObjectInputStream iVirta;
 	// Y:lt‰ saatava s‰ikeiden lukum‰‰r‰
-	private int t=0;
+	private int t;
 	
 	/*
 	 * Konstruktori
 	 */
 	public SummausPalvelin(){
-		
+		YPORTTI=3126;
+		ZPORTTI=2000;
 	}
 	
 	/*
@@ -41,7 +45,7 @@ public class SummausPalvelin extends Thread {
 	@Override
 	public void run(){
 		int i=0;
-		leima1 : while (i<5){ //yritet‰‰n viisi kertaa
+		while (i<5){ //yritet‰‰n viisi kertaa
 			i++;
 			muodostaUDP();
 			try {							// Odotetaan 1 sec
@@ -52,56 +56,71 @@ public class SummausPalvelin extends Thread {
 			try {
 				kuunteleTCPMuodostus();
 			} catch (SocketException e) {
-				continue leima1; // aikaraja meni, yritet‰‰n uudelleen, jos viel‰ kertoja j‰ljell‰
+				continue; 					// aikaraja meni, yritet‰‰n uudelleen, jos viel‰ kertoja j‰ljell‰
 			}
+			long time1=System.currentTimeMillis();
 			while(t<2 || t>10){
-				//TODO oikea viiden sekunnin aikaraja
-				t=kuunteleLuku(5000); //Y:n pit‰isi l‰hett‰‰ palvelijoiden lukum‰‰r‰
-				if (t==-1){ //ei tullut oikeaa lukua oikeassa ajassa
-					lahetaLuku(-1); //Y:lle -1
-					lopetaKokoPaska(); // Hallittu lopetus
+				long time2=System.currentTimeMillis();
+				int aikaraja=5000-(int)(time2-time1);
+				if(aikaraja>0){
+					try{
+						t=kuunteleLuku(aikaraja); 		// Y:n pit‰isi l‰hett‰‰ palvelijoiden lukum‰‰r‰
+					}
+					catch(SocketTimeoutException ste){	// ei tullut oikeaa lukua oikeassa ajassa
+						lahetaLuku(-1); 	// Y:lle -1
+						lopetaKokoPaska(); 	// Hallittu lopetus
+					}
+				}
+				else{						// ei tullut oikeaa lukua oikeassa ajassa
+					lahetaLuku(-1); 		// Y:lle -1
+					lopetaKokoPaska(); 		// Hallittu lopetus
 				}
 			}
-			System.out.println("Palvelijoiden lukum‰‰r‰ "+t);
+//			System.out.println("Palvelijoiden lukum‰‰r‰ "+t);
 			luoSummausPalvelijat();
-			for (int p=2001;p<2001+t;p++){ //Porttien l‰hetys Y:lle
+			for (int p=2001; p<2001+t; p++){ 	// Porttien l‰hetys Y:lle
 				lahetaLuku(p);
 			}
 			while(true){
-				int a=kuunteleLuku(10000); //Y:n kysely
-				if(a==1){		// Kokonaissumma
-					int b=0; //v‰lisuumma, johon lis‰t‰‰n jokaisen s‰ikeen summa
-					for(Summalista s : summaTaulukko){
-						b+=s.getSisalto()[0];
-					}
-					lahetaLuku(b);
-				}
-				else if(a==2){  // S‰ie, jolla suurin summa
-					System.out.println("Kysyt‰‰n s‰ijett‰, jolla suurin summa.");
-					tulosta();
-					int c=0;
-					int b=summaTaulukko[0].getSisalto()[0];
-					for(int j=1; j<t; j++){
-						if(summaTaulukko[j].getSisalto()[0]>b){
-							b=summaTaulukko[j].getSisalto()[0];
-							c=j;
+				try{
+					int a=kuunteleLuku(60000); 	// Y:n kysely, aikaraja 1 min
+					if(a==1){					// Kokonaissumma
+						int b=0; 				// v‰lisuumma, johon lis‰t‰‰n jokaisen s‰ikeen summa
+						for(Summalista s : summaTaulukko){
+							b+=s.getSisalto()[0];
 						}
+						lahetaLuku(b);
 					}
-					System.out.println("S‰ije "+(c+1));
-					lahetaLuku(c+1); //Y:lle s‰ikeen indeksi+1
-				}
-				else if(a==3){  // Kokonaism‰‰r‰
-					int b=0;
-					for(Summalista s : summaTaulukko){
-						b+=s.getSisalto()[1];
+					else if(a==2){  			// S‰ie, jolla suurin summa
+	//					System.out.println("Kysyt‰‰n s‰iett‰, jolla suurin summa.");
+	//					tulosta();
+						int c=0;
+						int b=summaTaulukko[0].getSisalto()[0];
+						for(int j=1; j<t; j++){
+							if(summaTaulukko[j].getSisalto()[0]>b){
+								b=summaTaulukko[j].getSisalto()[0];
+								c=j;
+							}
+						}
+	//					System.out.println("S‰ie "+(c+1));
+						lahetaLuku(c+1); 		// Y:lle s‰ikeen indeksi+1
 					}
-					lahetaLuku(b);
+					else if(a==3){  // Kokonaism‰‰r‰
+						int b=0;
+						for(Summalista s : summaTaulukko){
+							b+=s.getSisalto()[1];
+						}
+						lahetaLuku(b);
+					}
+					else if(a==0){		// Y k‰skee lopettaa
+						lopetaKokoPaska();
+					}
+					else{
+						lahetaLuku(-1); // Jos luku on joku muu, Y:lle -1
+					}
 				}
-				else if(a==0 || a==-1){
+				catch(SocketTimeoutException ste){  // Aikaraja meni, lopetetaan
 					lopetaKokoPaska();
-				}
-				else{
-					lahetaLuku(-1); //Jos luku on joku muu, Y:lle -1
 				}
 			}
 		}
@@ -116,12 +135,12 @@ public class SummausPalvelin extends Thread {
 	private void muodostaUDP(){
 		try {
 			DatagramSocket udp=new DatagramSocket();
-			byte[] tavu=(""+zPortti).getBytes();
+			byte[] tavu=(""+ZPORTTI).getBytes();
 //			for (byte b : tavu){
 //				System.out.println("Tavu on "+b);
 //			}
 			InetAddress omaIP=InetAddress.getLocalHost();
-			DatagramPacket paketti=new DatagramPacket(tavu, tavu.length, omaIP, yPortti);
+			DatagramPacket paketti=new DatagramPacket(tavu, tavu.length, omaIP, YPORTTI);
 			udp.send(paketti);
 			udp.close();
 		} catch (SocketException e) {
@@ -160,7 +179,7 @@ public class SummausPalvelin extends Thread {
 	 */
 	private void kuunteleTCPMuodostus() throws SocketException {
 		try {
-			ServerSocket soketti=new ServerSocket(zPortti);
+			ServerSocket soketti=new ServerSocket(ZPORTTI);
 			soketti.setSoTimeout(5000);
 			asiakasSoketti=soketti.accept();
 			soketti.close();
@@ -178,23 +197,25 @@ public class SummausPalvelin extends Thread {
 
 	/*
 	 * Kuuntelee ja palauttaa TCP-yhteyden kautta saadun kokonaisluvun.
-	 * Jos aikarajan kuluessa ei tule kokonaislukua palautetaan -1.
+	 * Jos aikarajan kuluessa ei tule kokonaislukua, heitet‰‰n poikkeus
 	 * 
 	 * @param aikaraja: aika millisekunteina, joka odotetaan kunnes lopetetaan
-	 * @return TCP-yhteyden kautta saatu kokonaisluku, tai -1 jos aikaraja ylittyi
+	 * @return TCP-yhteyden kautta saatu kokonaisluku
+	 * @throw SocketTimeoutException jos aikaraja ylittyi
 	 */
-	private int kuunteleLuku(int aikaraja) {
-		int luku=-1;
+	private int kuunteleLuku(int aikaraja) throws SocketTimeoutException {
+		int luku=-1;		// T‰t‰ ei koskaan palauteta
 		try {
 			asiakasSoketti.setSoTimeout(aikaraja);
 			luku=(int)iVirta.readInt();
 		}
 		catch (SocketTimeoutException e1){ //Aika loppu
-			return luku;
+			throw new SocketTimeoutException("Aika loppu");
 		}
 		catch (IOException e) { //Odottematon virhe, mit‰ tehd‰?
 			// TODO ratkaisu
 			e.printStackTrace();
+			lopetaKokoPaska();
 		}
 		return luku;
 	}
@@ -203,7 +224,7 @@ public class SummausPalvelin extends Thread {
 	/*
 	 * L‰hett‰‰ kokonaisluvun Y:lle.
 	 * K‰ytt‰‰ TCP-yhteytt‰
-	 * @param l‰hetett‰v‰ kokonaisluku luku
+	 * @param luku l‰hetett‰v‰ kokonaisluku
 	 */
 	private void lahetaLuku(int luku) {
 		try {
@@ -238,10 +259,10 @@ public class SummausPalvelin extends Thread {
 	/*
 	 * Tulostaa tilantaan
 	 */
-	public void tulosta(){
-		for (int i=0;i<t;i++){
-			System.out.println("Summa: "+summaTaulukko[i].getSisalto()[0]);
-			System.out.println("Lukum‰‰r‰: "+summaTaulukko[i].getSisalto()[1]);
-		}
-	}
+//	public void tulosta(){
+//		for (int i=0;i<t;i++){
+//			System.out.println("Summa: "+summaTaulukko[i].getSisalto()[0]);
+//			System.out.println("Lukum‰‰r‰: "+summaTaulukko[i].getSisalto()[1]);
+//		}
+//	}
 }
